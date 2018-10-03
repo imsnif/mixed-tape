88a04411329f2a3a95d958bcfda3cb5063cd5aa2
Aram Drevekenin <grimsniffer@gmail.com>
2018-10-03 11:36:59 +0200

    fix(registration): debounced registration hook

--- index.js
+++ index.js
@@ -1,20 +1,22 @@
+const debounce = require('debounce')
 const { api } = require('./lib/api')
 const { createReport } = require('./lib/create-report')
 
 module.exports = tape => {
+  process.stdout = process.stdout || require('browser-stdout')()
+  const registerTest = debounce(
+    () => Promise.all(state.testsEnded).then(() => report.end()), 100
+  )
   let state = {
     only: false,
     pipedToProcess: false,
     testsEnded: [],
     runOnFailure: [],
-    runOnFinish: []
+    runOnFinish: [],
+    registerTest
   }
   const report = createReport(state)
   tape.createStream() // filter out all tape output
   report.pipe(process.stdout)
-  process.nextTick(
-    () => Promise.all(state.testsEnded).then(() => report.end())
-    // this will break if tests are created asynchronously
-  )
   return api(state, tape, report)
 }
